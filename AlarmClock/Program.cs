7eb5dbcb4927c73b016647350b30ff36622b6078
LowLatency <Dennis.Moy@advancedtech-usa.com>
2015-05-20 12:08:32 -0400

    Add input interface and program exit feature.

--- AlarmClock/Program.cs
+++ AlarmClock/Program.cs
@@ -12,23 +12,45 @@ namespace AlarmClock
         static void Main(string[] args)
         {
 
+            var startTime = DateTime.Now;
+
             TimeSpan interval = new TimeSpan(0, 0, 1);
 
-            while (true)
+            //User input
+            Console.WriteLine("Alarm Clock v1.0");
+            Console.WriteLine("Alarm 1");
+
+            //Hour input
+            Console.Write("Hour: ");
+            double hr = double.Parse(Console.ReadLine());
+            Console.WriteLine("You have entered: {0}", hr);
+
+            //Minute input
+            Console.Write("Minute: ");
+            double min = double.Parse(Console.ReadLine());
+            Console.WriteLine("You have entered: {0}", min);
+
+            Thread.Sleep(new TimeSpan(0, 0, 3));
+            Console.Clear();
+
+
+            //Time testing
+            do
             {
-                string Text = DateTime.Now.ToString();
-                Console.WriteLine(Text);
-                Thread.Sleep(interval);
-                Console.Clear();
+                while (! Console.KeyAvailable)
+                {
+                    string Text = DateTime.Now.ToString();
+                    Console.WriteLine(startTime);
+                    Console.WriteLine(Text);
+                    Console.WriteLine("\n\nPress ESC to exit");
 
-            }
-            
+                    Thread.Sleep(interval);
+                    Console.Clear();
+                }
+            } while (Console.ReadKey(true).Key != ConsoleKey.Escape);
 
-            
 
 
-
-            Console.Read();
         }
     }
 }
